bf81ba00626694da49c814ff34f973c348aec1ab
thebroonclan <37247535+thebroonclan@users.noreply.github.com>
2025-07-24 22:39:55 +0100

    Update F

--- open-next.config.mjs
+++ open-next.config.mjs
@@ -1,5 +1,12 @@
-export {
-    function: {
-        runtime: 'clsoudflare'
-    }
-}
+/** @type {import("open-next").OpenNextConfig} */
+export default {
+  output: {
+    cloudflare: {
+      name: "johnathan-next", // any name is fine
+    },
+  },
+  functionOptions: {
+    wrapper: "default",
+    converter: "default",
+  },
+};
